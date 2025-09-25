f4d8645f8566bed519dc1fecc1db4074a33bc6dc
se7en-6xt9 <madhusudanrajpur2000@gmail.com>
2025-09-25 22:53:19 +0530

    o

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+  int main() {
+ printf("This code is to print even/odd number\n");
+ int num;
+ printf("Enter number : ");
+ scanf("%d", &num);
+ if(num%2==0){
+	printf("Number is even\n");
+	} else{
+	printf("Number is odd");
+	} 
+	 }
